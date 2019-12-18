b73290de03e129ec7349602a7b7e6e751e136992
IsraelSanabriaMx <israel.sanabriamx@gmail.com>
2019-12-18 09:56:53 -0600

    Use different style

--- miscellaneous/src/app/components/ng-style/ng-style.component.ts
+++ miscellaneous/src/app/components/ng-style/ng-style.component.ts
@@ -3,17 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ng-style',
   template: `
-    <p [ngStyle]="{ 'font-size': '20px' }">
+    <p [style.fontSize.px]="size">
       Hola mundo
     </p>
+    <button class="btn btn-primary" (click)="size = size + 5">
+      <i class="fas fa-plus-circle"></i>
+    </button>
+    <button class="btn btn-danger" (click)="size = size - 5">
+      <i class="fas fa-minus-circle"></i>
+    </button>
   `,
   styles: []
 })
 export class NgStyleComponent implements OnInit {
+  size = 20;
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
