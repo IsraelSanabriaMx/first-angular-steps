4ba1256904a2cbcae6ba96de75f8cfa650a5caf4
IsraelSanabriaMx <israel.sanabriamx@gmail.com>
2019-12-18 21:16:11 -0600

    Create classesComponent

---
+++ miscellaneous/src/app/components/classes/classes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-classes',
+  templateUrl: './classes.component.html',
+  styles: []
+})
+export class ClassesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
