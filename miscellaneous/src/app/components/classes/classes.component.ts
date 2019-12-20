20c5b7150ea6f1730933072d92bb8bd0cdd5a5b1
IsraelSanabriaMx <israel.sanabriamx@gmail.com>
2019-12-20 13:13:46 -0600

    Add ngClass functionality

--- miscellaneous/src/app/components/classes/classes.component.ts
+++ miscellaneous/src/app/components/classes/classes.component.ts
@@ -2,14 +2,16 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-classes',
-  templateUrl: './classes.component.html',
-  styles: []
+  templateUrl: './classes.component.html'
 })
 export class ClassesComponent implements OnInit {
+  alert = 'alert-danger';
+  properties = {
+    danger: true,
+  };
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
